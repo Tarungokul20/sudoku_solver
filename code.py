e5bccb3fbaadcc6828e790e079445cf32e2c7ce5
Tarun Gokul <tarungokul2007@gmail.com>
2025-07-21 17:28:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,94 +1,76 @@
-def add_time(start, duration, day=None):
-    # Days of the week
-    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+class Board:
+    def __init__(self, board):
+        self.board = board
 
-    # Split and parse start time
-    time_part, period = start.split()
-    start_hour, start_minute = map(int, time_part.split(':'))
+    def __str__(self):
+        board_str = ''
+        for row in self.board:
+            row_str = [str(i) if i else '*' for i in row]
+            board_str += ' '.join(row_str)
+            board_str += '\n'
+        return board_str
 
-    # Convert start time to 24-hour format
-    if period == "PM" and start_hour != 12:
-        start_hour += 12
-    elif period == "AM" and start_hour == 12:
-        start_hour = 0
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
 
-    # Parse duration
-    duration_hour, duration_minute = map(int, duration.split(':'))
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
 
-    # Total minutes
-    total_minutes = start_minute + duration_minute
-    extra_hour = total_minutes // 60
-    final_minutes = total_minutes % 60
+    def valid_in_col(self, col, num):
+        return all(self.board[row][col] != num for row in range(9))
 
-    # Total hours
-    total_hours = start_hour + duration_hour + extra_hour
-    days_later = total_hours // 24
-    final_hour_24 = total_hours % 24
+    def valid_in_square(self, row, col, num):
+        row_start = (row // 3) * 3
+        col_start = (col // 3) * 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3):
+                if self.board[row_no][col_no] == num:
+                    return False
+        return True
 
-    # Convert back to 12-hour format
-    if final_hour_24 == 0:
-        final_hour = 12
-        final_period = "AM"
-    elif final_hour_24 < 12:
-        final_hour = final_hour_24
-        final_period = "AM"
-    elif final_hour_24 == 12:
-        final_hour = 12
-        final_period = "PM"
+    def is_valid(self, empty, num):
+        row, col = empty
+        valid_in_row = self.valid_in_row(row, num)
+        valid_in_col = self.valid_in_col(col, num)
+        valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
+
+    def solver(self):
+        if (next_empty := self.find_empty_cell()) is None:
+            return True
+        for guess in range(1, 10):
+            if self.is_valid(next_empty, guess):
+                row, col = next_empty
+                self.board[row][col] = guess
+                if self.solver():
+                    return True
+                self.board[row][col] = 0
+        return False
+
+def solve_sudoku(board):
+    gameboard = Board(board)
+    print(f'Puzzle to solve:\n{gameboard}')
+    if gameboard.solver():
+        print(f'Solved puzzle:\n{gameboard}')
     else:
-        final_hour = final_hour_24 - 12
-        final_period = "PM"
+        print('The provided puzzle is unsolvable.')
+    return gameboard
 
-    # Format minutes with leading zero
-    final_minutes_str = f"{final_minutes:02d}"
-
-    # Calculate new day if provided
-    if day:
-        start_day_index = days_of_week.index(day.capitalize())
-        new_day_index = (start_day_index + days_later) % 7
-        new_day = days_of_week[new_day_index]
-        day_string = f", {new_day}"
-    else:
-        day_string = ""
-
-    # Add day count message
-    if days_later == 1:
-        later_string = " (next day)"
-    elif days_later > 1:
-        later_string = f" ({days_later} days later)"
-    else:
-        later_string = ""
-
-    # Build final result
-    result = f"{final_hour}:{final_minutes_str} {final_period}{day_string}{later_string}"
-    return result
-
-print(add_time('3:30 PM', '2:12'))
-# Returns: 5:42 PM
-
-print(add_time('11:55 AM', '3:12'))
-# Returns: 3:07 PM
-
-print(add_time('2:59 AM', '24:00'))
-# Returns: 2:59 AM (next day)
-
-print(add_time('11:59 PM', '24:05'))
-# Returns: 12:04 AM (2 days later)
-
-print(add_time('8:16 PM', '466:02'))
-# Returns: 6:18 AM (20 days later)
-
-print(add_time('3:30 PM', '2:12', 'Monday'))
-# Returns: 5:42 PM, Monday
-
-print(add_time('2:59 AM', '24:00', 'saturDay'))
-# Returns: 2:59 AM, Sunday (next day)
-
-print(add_time('11:59 PM', '24:05', 'Wednesday'))
-# Returns: 12:04 AM, Friday (2 days later)
-
-print(add_time('8:16 PM', '466:02', 'tuesday'))
-# Returns: 6:18 AM, Monday (20 days later)
-
-print(add_time('3:00 PM', '0:00'))
-# Returns: 3:00 PM
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+solve_sudoku(puzzle)
